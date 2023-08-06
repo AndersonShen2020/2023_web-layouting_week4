5e7d51b8ac91b62301af107d47f505f8da6cc4ad
AndersonShen <shen.andersonshen@gmail.com>
2023-08-06 16:56:40 +0800

    feat: 增加 FAQ 的 RWD 平板

--- src/views/FaqView.vue
+++ src/views/FaqView.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="mt-24 mb-20">
+  <div class="mt-24 mt-md-20 mb-20">
     <div class="container">
       <div class="row flex-column text-black">
         <h2 class="title-sub">常見問題</h2>
-        <div class="col-2 mt-8">
+        <div class="col-2 col-md-4 mt-8">
           <p class="fz-24 fw-bold">我想詢問配鏡問題</p>
         </div>
-        <div class="col-8 mt-6">
+        <div class="col-8 col-md-12 mt-6">
           <p class="fz-20 fw-bold">Q1.請問可否自備鏡框單配鏡片？</p>
           <p class="mt-2">
             A1.我牌鏡框搭配薄型非球面鏡片 1480 元，搭配功能型鏡片則依鏡片種類加價購買。
@@ -16,7 +16,7 @@
             元，搭配功能型鏡片則依鏡片種類加價購買。
           </p>
         </div>
-        <div class="col-8 mt-8">
+        <div class="col-8 col-md-12 mt-8">
           <p class="fz-20 fw-bold">Q2.眼鏡都可以20分鐘取件嗎？</p>
           <p class="mt-2">
             A1.我牌鏡框搭配薄型非球面鏡片 1480 元，搭配功能型鏡片則依鏡片種類加價購買。
@@ -26,7 +26,7 @@
             元，搭配功能型鏡片則依鏡片種類加價購買。
           </p>
         </div>
-        <div class="col-8 mt-8">
+        <div class="col-8 col-md-12 mt-8">
           <p class="fz-20 fw-bold">Q3.散光鏡片需要額外加價嗎？</p>
           <p class="mt-2">
             A1.我牌鏡框搭配薄型非球面鏡片 1480 元，搭配功能型鏡片則依鏡片種類加價購買。
@@ -36,7 +36,7 @@
             元，搭配功能型鏡片則依鏡片種類加價購買。
           </p>
         </div>
-        <div class="col-8 mt-8">
+        <div class="col-8 col-md-12 mt-8">
           <p class="fz-20 fw-bold">Q4.我可以使用舊眼鏡的度數配鏡片嗎？</p>
           <p class="mt-2">
             A1.我牌鏡框搭配薄型非球面鏡片 1480 元，搭配功能型鏡片則依鏡片種類加價購買。
@@ -46,7 +46,7 @@
             元，搭配功能型鏡片則依鏡片種類加價購買。
           </p>
         </div>
-        <div class="col-8 mt-8">
+        <div class="col-8 col-md-12 mt-8">
           <p class="fz-20 fw-bold">Q5.請問可以單購買鏡框嗎？</p>
           <p class="mt-2">
             A1.我牌鏡框搭配薄型非球面鏡片 1480 元，搭配功能型鏡片則依鏡片種類加價購買。
