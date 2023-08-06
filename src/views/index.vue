65741b0900c32045f03e52a5b803f8b188423c1c
AndersonShen <shen.andersonshen@gmail.com>
2023-08-06 15:54:19 +0800

    style: formatting

--- src/views/index.vue
+++ src/views/index.vue
@@ -10,40 +10,29 @@
     </div>
 
     <!-- 用專業的心，做專業的事 -->
-    <div class="info bg-secondary text-secondary py-80 py-sm-40">
+    <div class="info bg-secondary text-secondary py-20 py-sm-40">
       <div class="container">
         <h2 class="text-align-center mb-6">用專業的心，做專業的事</h2>
         <div class="row flex-wrap">
-          <div
-            class="col-3 col-md-6 col-sm-4 d-flex flex-column align-items-center mb-md-6"
-          >
+          <div class="col-3 col-md-6 col-sm-4 d-flex flex-column align-items-center mb-md-6">
             <img src="@/assets/img/home-section2-1.png" alt="" />
             <p class="info-subtitle">單一價格</p>
             <p>無論任何度數皆不需追加費用即可擁有適合自己的薄型球面鏡片。</p>
           </div>
-          <div
-            class="col-3 col-md-6 col-sm-4 d-flex flex-column align-items-center mb-md-6"
-          >
+          <div class="col-3 col-md-6 col-sm-4 d-flex flex-column align-items-center mb-md-6">
             <img src="@/assets/img/home-section2-2.png" alt="" />
             <p class="info-subtitle">20 分鐘即可取件</p>
             <p>
-              為了您的寶貴時間著想，以豐富專業知識與技術將結帳到交件的時間縮減至最快
-              20 分鐘即可完成。
+              為了您的寶貴時間著想，以豐富專業知識與技術將結帳到交件的時間縮減至最快 20
+              分鐘即可完成。
             </p>
           </div>
-          <div
-            class="col-3 col-md-6 col-sm-4 d-flex flex-column align-items-center mb-md-6"
-          >
+          <div class="col-3 col-md-6 col-sm-4 d-flex flex-column align-items-center mb-md-6">
             <img src="@/assets/img/home-section2-3.png" alt="" />
             <p class="info-subtitle">安心售後服務</p>
-            <p>
-              我們提供長達 120
-              天的保固售後服務，不限會員資格皆享有免費深層保養及專業維修服務。
-            </p>
+            <p>我們提供長達 120 天的保固售後服務，不限會員資格皆享有免費深層保養及專業維修服務。</p>
           </div>
-          <div
-            class="col-3 col-md-6 col-sm-4 d-flex flex-column align-items-center"
-          >
+          <div class="col-3 col-md-6 col-sm-4 d-flex flex-column align-items-center">
             <img src="@/assets/img/home-sectioin2-4.png" alt="" />
             <p class="info-subtitle">單一價格</p>
             <p>使用世界知名頂級品牌，抗UV、防汙鍍膜薄型非球面鏡片。</p>
@@ -53,9 +42,9 @@
     </div>
 
     <!-- 經典系列鏡框 -->
-    <div class="text-secondary text-align-center py-80 py-sm-40">
+    <div class="text-secondary text-align-center py-20 py-sm-40">
       <div class="container">
-        <h2 class="mb-48 mb-sm-6">經典系列鏡框</h2>
+        <h2 class="mb-12 mb-sm-6">經典系列鏡框</h2>
         <div class="row flex-md-column align-items-md-center">
           <div class="col-4 col-md-6 col-sm-4 mb-md-6">
             <img class="mb-2" src="@/assets/img/home-section3-1.png" alt="" />
@@ -74,9 +63,9 @@
     </div>
 
     <!-- 聯名設計鏡框 -->
-    <div class="text-align-center py-80 py-sm-40 bg-index">
+    <div class="text-align-center py-20 py-sm-40 bg-index">
       <div class="container">
-        <h2 class="mb-48 mb-sm-6">聯名設計鏡框</h2>
+        <h2 class="mb-12 mb-sm-6">聯名設計鏡框</h2>
         <div class="row flex-md-column align-items-md-center">
           <div class="col-6 col-md-12 col-sm-4 mb-md-6">
             <img src="@/assets/img/home-section4-1.png" alt="" />
@@ -91,9 +80,9 @@
     </div>
 
     <!-- 顧客推薦 -->
-    <div class="py-80 py-sm-40">
+    <div class="py-20 py-sm-40">
       <div class="container">
-        <h2 class="text-secondary text-align-center mb-48 mb-sm-6">顧客推薦</h2>
+        <h2 class="text-secondary text-align-center mb-12 mb-sm-6">顧客推薦</h2>
         <ul class="row flex-wrap">
           <li class="col-3 col-md-6 col-sm-4 mb-md-6">
             <div class="box-shadow">
@@ -113,9 +102,7 @@
               <div class="d-flex flex-column justify-content-between p-4 h-205">
                 <div class="customer-info text-black">
                   <p class="2">凱倫</p>
-                  <p>
-                    做工細緻、鏡架很輕盈，待久也不會覺得有負擔，推薦給大家！
-                  </p>
+                  <p>做工細緻、鏡架很輕盈，待久也不會覺得有負擔，推薦給大家！</p>
                 </div>
                 <p class="text-gray">2021/04/18</p>
               </div>
@@ -127,9 +114,7 @@
               <div class="d-flex flex-column justify-content-between p-4 h-205">
                 <div class="customer-info text-black">
                   <p class="2">悠悠</p>
-                  <p>
-                    謝謝客服人員的詳細回答，成功買到了喜歡的眼鏡，下次會再回購！
-                  </p>
+                  <p>謝謝客服人員的詳細回答，成功買到了喜歡的眼鏡，下次會再回購！</p>
                 </div>
                 <p class="text-gray">2020/12/25</p>
               </div>
@@ -152,9 +137,9 @@
     </div>
 
     <!-- 聯絡我們 -->
-    <div class="py-80 py-sm-40 bg-secondary">
+    <div class="py-20 py-sm-40 bg-secondary">
       <div class="container">
-        <h2 class="text-secondary text-align-center mb-48 mb-sm-6">聯絡我們</h2>
+        <h2 class="text-secondary text-align-center mb-12 mb-sm-6">聯絡我們</h2>
         <div class="row justify-content-center text-black">
           <div class="col-6 col-md-10 col-sm-4">
             <p class="fz-20 fz-md-16 mb-6">
@@ -166,21 +151,11 @@
           <div class="row flex-column align-items-center text-black">
             <div class="col-6 col-md-10 col-sm-4 mb-4">
               <label class="mb-2" for="name">姓名</label>
-              <input
-                class="d-block w-100"
-                type="text"
-                id="name"
-                placeholder="陳小明"
-              />
+              <input class="d-block w-100" type="text" id="name" placeholder="陳小明" />
             </div>
             <div class="col-6 col-md-10 col-sm-4 mb-4">
               <label class="mb-2" for="phone">聯絡電話</label>
-              <input
-                class="d-block w-100"
-                type="tel"
-                id="phone"
-                placeholder="0912-345-678"
-              />
+              <input class="d-block w-100" type="tel" id="phone" placeholder="0912-345-678" />
             </div>
             <div class="col-6 col-md-10 col-sm-4 mb-4">
               <label class="mb-2" for="email">電子郵件</label>
@@ -202,13 +177,8 @@
                 placeholder="請輸入您的意見"
               ></textarea>
             </div>
-            <div class="col-6 col-md-10 col-sm-4 mb-48 mb-sm-6">
-              <input
-                type="checkbox"
-                class="input-check"
-                name="check"
-                id="check"
-              />
+            <div class="col-6 col-md-10 col-sm-4 mb-12 mb-sm-6">
+              <input type="checkbox" class="input-check" name="check" id="check" />
               <label class="mb-2" for="check"
                 >我同意隱私權政策，並同意依隱私權政策中所述的方式處理自己的資料。</label
               >
