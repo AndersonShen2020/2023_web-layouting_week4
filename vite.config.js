6827f0bfa465e15ac3a99e5065a08ad4a88ce144
AndersonShen <shen.andersonshen@gmail.com>
2023-08-10 17:17:44 +0800

    feat: 增加 vite.config 的 base

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/2023_web-layouting_week4/',
   plugins: [vue()],
   resolve: {
     alias: {
